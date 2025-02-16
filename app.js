1759a46c759e960cf01cfd0a83858072e65883e1
Rg96Dev <rafalgui96@gmail.com>
2025-02-16 18:29:50 -0300

    adicionando a funcinalidade finais para melhor funcionamento do programa

--- app.js
+++ app.js
@@ -12,20 +12,43 @@ function adicionarAmigo() {
     } else {
         amigos.push(nome);
         input.value = "";
-        
+
         atualizarLista();
     }
 }
 
 //função que cria lista do sorteio
 function atualizarLista() {
-    lista.innerHTML = ""; // Limpa a lista antes de atualizar
+    lista.innerHTML = "";
 
-    amigos.forEach((nome) => {
+    for (let i = 0; i < amigos.length; i++) {
         const itemLista = document.createElement("li");
-        itemLista.textContent = nome;
+        itemLista.textContent = amigos[i];
+
         lista.appendChild(itemLista);
-    });
+    }
+}
+
+//função para limpar lista
+function limparLista() {
+    let listaHtml = document.querySelector("ul");
+    listaHtml.innerHTML = "";
+    amigos.length = 0;
+    atualizarLista();
+
+}
+
+//função que faz o sorteio acontecer
+function sortearAmigo() {
+    if (amigos.length <= 1) {
+        alert("Número de amigos inválido! Insira 2 ou mais nomes para que sorteio seja excutado")
+    } else {
+        let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+        let parabensMensagem = document.querySelector('h2');
+        parabensMensagem.textContent = (`${amigoSorteado} é o seu amigo(a) secreto`);
+        limparLista();
+    }
+
 }
 
 
