a9ea89bd6bdbc2715c9944be2ff7d062ac50b6d1
Rg96Dev <rafalgui96@gmail.com>
2025-02-16 16:54:08 -0300

    adicionando funcionalidade e criando lista para sorteio

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
-// Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let lista = document.getElementById("listaAmigos");
+
+//função que pega os nomes e adiciona ao array 
+function adicionarAmigo() {
+    const input = document.getElementById("amigo");
+    const nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Insira um nome válido");
+
+    } else {
+        amigos.push(nome);
+        input.value = "";
+        
+        atualizarLista();
+    }
+}
+
+//função que cria lista do sorteio
+function atualizarLista() {
+    lista.innerHTML = ""; // Limpa a lista antes de atualizar
+
+    amigos.forEach((nome) => {
+        const itemLista = document.createElement("li");
+        itemLista.textContent = nome;
+        lista.appendChild(itemLista);
+    });
+}
+
+
+
